46cf3c8e6ecffc468d3847dcf676ebb264701bf4
Jeet <jeet.zaper@gmail.com>
2026-06-13 12:53:51 +0530

    updated Jenkins File

--- Interview_Preparation_2026/Python_Coding/asad.py
+++ Interview_Preparation_2026/Python_Coding/asad.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-
-current_script = Path(__file__).resolve()
-
-# 2. Go up 2 levels: Python_Coding -> Interview_Preparation_2026 -> CGPlaywrightAPIAutomation
-project_root = current_script.parents[2]
-
-print(project_root)
-actual_path =str(project_root) + r"\Learn_Playwright_BDD_Framework\features\OrderTransaction.feature"
-print(actual_path)
+import os
+print(os.path.join("Learn_Playwright_BDD_Framework", "features", "OrderTransaction.feature"))
+# current_script = Path(__file__).resolve()
+#
+# # 2. Go up 2 levels: Python_Coding -> Interview_Preparation_2026 -> CGPlaywrightAPIAutomation
+# project_root = current_script.parents[2]
+#
+# print(project_root)
+# actual_path =str(project_root) + r"\Learn_Playwright_BDD_Framework\features\OrderTransaction.feature"
+# print(actual_path)
